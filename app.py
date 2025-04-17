451b477f92689948a55067e2b86abf5f60038931
DAPEUDJOKAM <130173135+DAPEUDJOKAM@users.noreply.github.com>
2025-04-17 12:56:56 +0000

    nambah bootstrap

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': 'Rp. 10,000,000'
+    },
+    {
+        'id': 2,
+        'title': 'Backend Engineer',
+        'location': 'Bandung, Indonesia',
+        'salary': 'Rp. 10,000,000'
+    },
+    {
+        'id': 3,
+        'title': 'Video Editing',
+        'location': 'Remote',
+        'salary': 'Rp. 10,000,000'
+    }
+]
+
 @app.route("/")
 def hello_dhafan():
-    return render_template('home.html')
+    return render_template('home.html' , 
+                           jobs=JOBS , 
+                           company_name= 'Dhafan')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
